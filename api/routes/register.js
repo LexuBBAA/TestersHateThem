46bc926d6a93235a5d468eea0c736a7b5c78a579
y0rg1 <y0rg1@github.com>
2018-03-24 15:16:37 +0200

    added login and register endpoints

---
+++ api/routes/register.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const db = require('../db.js');
+
+router.post('/', (req, res) => {
+    let email =       req.body.email;
+    let password =    req.body.password;
+    let name =        req.body.name;
+    let type =        req.body.type;
+    let description = req.body.description;
+
+    db.query(`SELECT register('${email}', '${password}', '${name}', '${type}', '${description}') AS status`, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                success: false,
+                message: 'register failed!'
+            });
+            return;
+        }
+
+        if (results[0].status == 0) {
+            res.json({
+                success: false,
+                message: 'invalid input!'
+            })
+        } else {
+            let user_id = results[0].status;
+            let token = jwt.sign({ user_id: user_id }, process.env.SECRET);
+
+            res.json({
+                success: true,
+                token: token
+            })
+        }
+    });
+})
+
+module.exports = router
